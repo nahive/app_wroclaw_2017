941f9de4edce7e379ef925e8d39d39b10c5e307f
nahive <nahive@wifi-host-235.wcss.wroc.pl>
2014-10-22 14:02:45 +0200

    working details
    no fully implemented yet

--- wroclaw_2017/NewsDetailViewController.swift
+++ wroclaw_2017/NewsDetailViewController.swift
@@ -14,6 +14,7 @@ class NewsDetailViewController: UIViewController {
     @IBOutlet weak var newsContent: UILabel!
     @IBOutlet weak var newsTitle: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var insideView: UIView!
     
     var imageVal = "";
     var dateVal = "";
@@ -29,27 +30,46 @@ class NewsDetailViewController: UIViewController {
         newsTitle.text = titleVal;
         newsContent.text = contentVal;
         
-       
-      //  newsImage.frame.origin.x = 0;
         // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(animated: Bool) {
         
-//        var scrollViewHeight: CGFloat = 0.0;
-//        for view in scrollView.subviews{
-//            scrollViewHeight += view.frame.size.height
-//        }
-//        scrollView.contentSize = CGSizeMake(320, scrollViewHeight);
+//
+          newsImage.frame = CGRectMake(0,0,view.frame.size.width,view.frame.size.height/3);
+        newsDate.frame = CGRectMake(5, view.frame.size.height/3+2, view.frame.size.width/2, newsDate.frame.size.height);
+        newsTitle.frame = CGRectMake(5, newsDate.frame.origin.y + newsDate.frame.size.height, view.frame.size.width/2, newsTitle.frame.size.height*3);
+        newsContent.frame = CGRectMake(5, newsTitle.frame.origin.y + newsTitle.frame.size.height, view.frame.size.width,newsContent.frame.size.height*10);
         
-        newsImage.frame = CGRectMake(87,0,view.frame.size.width,view.frame.size.height/3);
-        newsDate.frame = CGRectMake(95, view.frame.size.height/3+2, view.frame.size.width/2, newsDate.frame.size.height);
-        newsTitle.frame = CGRectMake(95, newsDate.frame.origin.y + newsDate.frame.size.height, view.frame.size.width/2, newsTitle.frame.size.height*3);
-        newsContent.frame = CGRectMake(95, newsTitle.frame.origin.y + newsTitle.frame.size.height, view.frame.size.width,newsContent.frame.size.height*100);
+//        var viewHeight: CGFloat = 0.0;
+//        for view in insideView.subviews{
+//            viewHeight += view.frame.size.height
+//        }
+//        insideView.frame.size.height = viewHeight;
+
+        var scrollViewHeight: CGFloat = 0.0;
+        for view in scrollView.subviews{
+            scrollViewHeight += view.frame.size.height
+        }
+        scrollView.contentSize = CGSizeMake(320, scrollViewHeight);
+
         
         
     }
     
+//    func resizeToFitSubviews(){
+//        var w : Int = 0;
+//        var h : Int = 0;
+//        
+//        for v in insideView.subviews {
+//            var fw = (v.frame.origin.x + v.frame.size.width);
+//            var fh = (v.frame.origin.y + v.frame.size.height);
+//            w = w > fw ? w : fw;
+//            h = h > fh ? h : fh;
+//        }
+//        insideView.frame = CGRectMake(insideView.origin.x, insideView.frame.origin.y, <#width: CGFloat#>, <#height: CGFloat#>)
+//    }
+//    
     override func viewWillAppear(animated: Bool) {
         //
         
