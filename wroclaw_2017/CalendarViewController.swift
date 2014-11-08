da1d8a8c4d7965d8d8be3922a6d7782b90e8f116
Adam <adam-privat@MBP-Adam.lan>
2014-11-08 14:24:01 +0100

    sections in calendar

--- wroclaw_2017/CalendarViewController.swift
+++ wroclaw_2017/CalendarViewController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class CalendarViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-  //  @IBOutlet weak var label: UILabel!
+class CalendarViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //  @IBOutlet weak var label: UILabel!
     @IBOutlet weak var datePicker: DIDatepicker!
     @IBOutlet weak var revealButtonItem: UIBarButtonItem!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var datePickerW: NSLayoutConstraint!
     
-    var events: [String: String] = ["Softball": "Men's Squash Final", "Tug of War": "Men's Fina", "Squash": "Men's Semifinal"];
     
+    var screen =  UIScreen.mainScreen().bounds;
     var times: [String] = ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","16:15","17:00","18:15"];
     var titles: [String] = ["Dance Sport Women's eliminations","Men's Squash Final","Ju-jitsu Men's 1st round","Archery Women's Semifinal","Tug of War Men's eliminations","Women's Duathlon","Rugby Sevens Men's Quarterfinal","Women's Air Sports","Men's Softball 2sd round","Canoe Polo Women's 3rd round","Billard Sports Men's eliminations","Korfball Women's Final"];
     var fullContents: [String] = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend malesuada arcu, tincidunt feugiat leo lacinia at. Nam felis metus, scelerisque ultrices metus quis, vulputate ultricies quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend malesuada arcu, tincidunt feugiat leo lacinia at. Nam felis metus, scelerisque ultrices metus quis, vulputate ultricies quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend malesuada arcu, tincidunt feugiat leo lacinia at. Nam felis metus, scelerisque ultrices metus quis, vulputate ultricies quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend malesuada arcu, tincidunt feugiat leo lacinia at. Nam felis metus, scelerisque ultrices metus quis, vulputate ultricies quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend malesuada arcu, tincidunt feugiat leo lacinia at. Nam felis metus, scelerisque ultrices metus quis, vulputate ultricies quam.",
@@ -25,8 +26,16 @@ class CalendarViewController: UIViewController, UITableViewDelegate, UITableView
         "Duis ut nulla interdum, malesuada justo nec, posuere purus. Mauris ac porta eros. Nulla finibus nisi sit amet commodo ullamcorper. Cras mollis tempor commodo. Integer quam tellus. Duis ut nulla interdum, malesuada justo nec, posuere purus. Mauris ac porta eros. Nulla finibus nisi sit amet commodo ullamcorper. Cras mollis tempor commodo. Integer quam tellus. Duis ut nulla interdum, malesuada justo nec, posuere purus. Mauris ac porta eros. Nulla finibus nisi sit amet commodo ullamcorper. Cras mollis tempor commodo. Integer quam tellus."];
     
     
-    var sportCategories: [String] = ["Softball", "Tug of War", "Squash"];
+    //How to add many values for one key?
+    var events: [String: String] = ["Softball": "Men's Squash Final", "Tug of War": "Men's Final", "Squash": "Men's Semifinal"];
+    var eventsSectionTitles: [String] = ["Softball", "Tug of War", "Squash"];
+    var eventIndexTitles: [String] = ["Softball", "Tug of War", "Squash", "Dance Sport"];
     
+    
+    
+    func backwards(s1: String, s2: String) -> Bool {
+        return s1 > s2
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,8 +44,11 @@ class CalendarViewController: UIViewController, UITableViewDelegate, UITableView
         self.tableView.alpha = 0.0;
         var scroll: UIScrollView! = datePicker.subviews[0] as UIScrollView;
         scroll.frame = CGRectMake(0, 0, view.frame.width, datePicker.frame.height);
-      
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func prepareTable() {
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -95,18 +107,64 @@ class CalendarViewController: UIViewController, UITableViewDelegate, UITableView
             destViewController.dateVal = dateStr;
         }
     }
-
+    
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return sportCategories.count;
+        return eventsSectionTitles.count;
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 50
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titles.count;
+        //        var sectionTitle: String = eventsSectionTitles.removeAtIndex(section);
+        //        eventsSectionTitles.insert(sectionTitle, atIndex: section);
+        //        println(sectionTitle);
+        //        var sectionEvents: String = events.removeValueForKey(sectionTitle)!;
+        //        println(sectionEvents);
+        return 1;
+        
     }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title: String = eventsSectionTitles.removeAtIndex(section);
+        eventsSectionTitles.insert(title, atIndex: section);
+        return title;
+        
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        //section header view
+        var sectionHeaderView: UIView = UIView();
+        sectionHeaderView.frame = CGRectMake(10, 10, screen.width, 30);
+        sectionHeaderView.backgroundColor = Utils.colorize(0xfafafa);
+        
+        
+        var disciplineIcon: UIImage = UIImage(named: "wushu.png")!;
+        var imageView: UIImageView = UIImageView(image: disciplineIcon);
+        imageView.frame = CGRectMake(10, 5, 30, 30);
+        imageView.backgroundColor = Utils.colorize(0x888888);
+        imageView.layer.cornerRadius = 8;
+        sectionHeaderView.addSubview(imageView);
+        
+        
+        var title: String = eventsSectionTitles.removeAtIndex(section);
+        eventsSectionTitles.insert(title, atIndex: section);
+        var label: UILabel = UILabel();
+        label.text = title;
+        label.font = UIFont(name: "HelveticaNeue-Thin", size: 21);
+        label.frame = CGRectMake(screen.width/2 - 50, 5, 100, 30);
+        label.textAlignment = NSTextAlignment.Center;
+        sectionHeaderView.addSubview(label);
+        
+        return sectionHeaderView;
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40;
+    }
+    
     
     
     
@@ -117,25 +175,42 @@ class CalendarViewController: UIViewController, UITableViewDelegate, UITableView
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: tableId);
         }
         var title: UILabel? = cell!.viewWithTag(201) as? UILabel;
-        title?.text = titles[indexPath.row];
-        title?.font = UIFont(name: "HelveticaNeue-Light", size: 17);
+        // title?.text = titles[indexPath.row];
+        //title?.font = UIFont(name: "HelveticaNeue-Light", size: 17);
         var time: UILabel? = cell!.viewWithTag(202) as? UILabel;
         time?.text = times[indexPath.row];
-        time?.font = UIFont(name: "HelveticaNeue-Thin", size: 15);
+        time?.font = UIFont(name: "HelveticaNeue-Thin", size: 12);
         
+        //        NSString *sectionTitle = [animalSectionTitles objectAtIndex:indexPath.section];
+        //        NSArray *sectionAnimals = [animals objectForKey:sectionTitle];
+        //        NSString *animal = [sectionAnimals objectAtIndex:indexPath.row];
+        //        cell.textLabel.text = animal;
+        
+        
+        //How to get object at index????
+        var sectionTitle: String = eventsSectionTitles.removeAtIndex(indexPath.section);
+        eventsSectionTitles.insert(sectionTitle, atIndex: indexPath.section);
+        
+        //This should return an array from which particural event should be taken
+        var sectionEvents: String = events.removeValueForKey(sectionTitle)!;
+        events.updateValue(sectionEvents, forKey: sectionTitle);
+        
+        title?.text = sectionEvents;
+        title?.font = UIFont(name: "HelveticaNeue-Light", size: 15);
         
         return cell!
     }
     
+    
     //
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
